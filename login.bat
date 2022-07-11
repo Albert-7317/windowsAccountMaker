dd24078561a90882dcebdc04a820914de5bbf505
Albert Brown <103575236+Albert-7317@users.noreply.github.com>
2022-07-11 10:24:51 +0000

    Update login.bat

--- login.bat
+++ login.bat
@@ -3,11 +3,19 @@
 FOR %%x IN (1 2 3) DO (
     ECHO %%x
     CLS
+    ECHO Starting.
+    TIMEOUT 1
+    CLS
+    ECHO Statring ..
+    TIMEOUT 1
+    CLS
+    ECHO Starting...
+    TIMEOUT 1
+    CLS
 )
 
-
-
 ::Variables
+ECHO Starting....
 COLOR 2
 SET /A time = 5
 
